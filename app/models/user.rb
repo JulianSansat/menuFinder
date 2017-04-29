8b82f7bf8c9f7a3c1c45bf255742f8e75333f90a
JulianSansat <kerarchie@yahoo.com.ar>
2017-04-29 18:16:43 -0300

    changed user validation for tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
   include DeviseTokenAuth::Concerns::User
 
-  validates :name, presence: true
+  #validates :name, presence: true
 
 end
